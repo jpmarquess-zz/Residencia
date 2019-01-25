ab459fc355642679e19b1d45688859c36cedc435
João Marques <jpmarques1099@gmail.com>
2019-01-25 21:59:21 +0000

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+$('.carousel').carousel({
+    interval: 2000
+});
+
+/*var $document = $(document);
+$document.scroll(function () {
+    $("header").toggleClass("fixed-top", $document.scrollTop() >= 300);
+});*/
